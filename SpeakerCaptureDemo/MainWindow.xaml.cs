df7b0d04924ab38bfbbf976609446489fd52f5be
kybs00 <1016724958@qq.com>
2024-08-24 17:50:29 +0800

    添加扬声器录制

--- SpeakerCaptureDemo/MainWindow.xaml.cs
+++ SpeakerCaptureDemo/MainWindow.xaml.cs
@@ -1,9 +1,8 @@
-﻿using System.Diagnostics;
-using System.Windows;
+﻿using System.Windows;
 using NAudio.CoreAudioApi;
 using NAudio.Wave;
 
-namespace MicCaptureDemo2
+namespace SpeakerCaptureDemo
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
@@ -19,16 +18,16 @@ namespace MicCaptureDemo2
         private WasapiCapture _capture;
         private void MainWindow_Loaded(object sender, RoutedEventArgs e)
         {
-            _capture = new WasapiCapture();
+            _capture = new WasapiLoopbackCapture();
             _writer = new WaveFileWriter("recordedAudio.wav", _capture.WaveFormat);
             _capture.DataAvailable += (s, a) =>
             {
                 _writer.Write(a.Buffer, 0, a.BytesRecorded);
             };
             // 列出所有可用的录音设备
-            for (int i = 0; i < WaveIn.DeviceCount; i++)
+            for (int i = 0; i < WaveOut.DeviceCount; i++)
             {
-                var deviceInfo = WaveIn.GetCapabilities(i);
+                var deviceInfo = WaveOut.GetCapabilities(i);
                 OutputTextBlock.Text += $"Device {i}: {deviceInfo.ProductName}\r\n";
             }
         }
